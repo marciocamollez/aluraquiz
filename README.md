161f75d410e727d63c98bcc3be213d5a89e2f523
Marcio <marciocamollez@hotmail.com>
2021-01-27 15:32:17 -0300

    Aula 2

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+1-Criar o projeto
+
+npx create-next-app --example with-styled-components
+
+
+2-Rodar o projeto
+
+npm run dev
+
+
+
+
 # Example app with styled-components
 
 This example features how you use a different styling solution than [styled-jsx](https://github.com/zeit/styled-jsx) that also supports universal styles. That means we can serve the required styles for the first render within the HTML and then load the rest in the client. In this case we are using [styled-components](https://github.com/styled-components/styled-components).
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,77 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
 `
 
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
-  return <Title>My page</Title>
+
+  const router = useRouter();
+  const [name, setName] = React.useState('');  
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+
+      <Head>
+        <title>Alura Quiz</title>
+      </Head>
+
+      <QuizContainer>
+        <QuizLogo />
+
+        <Widget>
+          <Widget.Header><h1>Legend of Zelda</h1></Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+                
+              router.push(`/quiz?name=${name}`);          
+            }}>
+              <input 
+                onChange={function (infosDoEvento){
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Insira seu nome" 
+              />
+              <button type="submit" disabled={name.length === 0}>Jogar {name}</button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+
+        <Widget>
+          <Widget.Header>ccc</Widget.Header>
+          <Widget.Content>
+            dddd
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+
+      </QuizContainer>
+
+      <GitHubCorner />
+    </QuizBackground>
+  );
 }
