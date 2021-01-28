9c2f6ee3ba184fe1863a88878bc8e1ef10ffc08f
Marcio <marciocamollez@hotmail.com>
2021-01-28 14:19:46 -0300

    Aula 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json';
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
+  * {
+    box-sizing: border-box;
   }
   body {
     margin: 0;
@@ -26,8 +27,9 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -37,8 +39,9 @@ export default function App({ Component, pageProps }) {
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
